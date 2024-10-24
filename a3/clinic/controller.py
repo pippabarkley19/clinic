900d464534df511e87ebb229d65f7db443f631fc
Pippa Barkley <pippabarkley@ugls6.ece.uvic.ca>
2024-10-24 12:38:19 -0700

    ACTUALLY implemented login/logout, forgot to save last file before pushing

--- a3/clinic/controller.py
+++ a3/clinic/controller.py
@@ -9,11 +9,24 @@ class Controller():
         self.patients = [] # List of Patient instances
         self.login = [] # List of Patient logins  
         self.current_patient = None 
+        self.username = "user"
+        self.password = "clinic2024"
+        self.loggedIn = False
 
-    # not sure if this will need an init
     # input: username, password
-    def login(self):
+    def login(self, user, pword):
+        if self.loggedIn:
+             return False
+        else:
+            if user == self.username and pword == self.password:
+                self.loggedIn = True
+                return True
+
 
     # input: none
     def logout(self):
-
+        if self.loggedIn:
+            self.loggedIn = False
+            return True
+        else:
+            return False

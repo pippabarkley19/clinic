eed4fb77328a18a27f692e4d1141dd9d5b87c94f
Pippa Barkley <pippabarkley@ugls27.ece.uvic.ca>
2024-11-01 10:20:41 -0700

    Tests finalized, all passing!

--- a3/tests/note_test.py
+++ a3/tests/note_test.py
@@ -1,13 +1,12 @@
 from unittest import TestCase
 from unittest import main
-from clinic.controller import Controller
-from clinic.patient import Patient
-from clinic.patient_record import PatientRecord
 from clinic.note import Note
 
 class NoteTests(TestCase):
 
     def test_note_equality(self):
+
+        #set up testing items
         note1 = Note(1, "Patient comes with headache and high blood pressure.")
         note2 = Note(2, "Patient complains of a strong headache on the back of neck.")
         note3 = Note(1, "Patient comes with headache and high blood pressure.")
@@ -18,11 +17,16 @@ class NoteTests(TestCase):
         self.assertFalse(note1 == note2)
 
     def test_note_str(self):
+
+        # set up testing items
         note1 = Note(1, "Patient comes with headache and high blood pressure.")
         note_expected = "Index: 1, Description: Patient comes with headache and high blood pressure."
+        note_not_expected = "Index: 2, Description: Patient comes with headache and high blood pressure."
 
-        # test str output
+        # test str correct output
         self.assertEqual(str(note1), note_expected)
+        # test str incorrect output
+        self.assertNotEqual(str(note1), note_not_expected)
 
 if __name__ == '__main__':
     main()
--- a3/tests/patient_record_test.py
+++ a3/tests/patient_record_test.py
@@ -1,20 +1,22 @@
 from unittest import TestCase
 from unittest import main
-from clinic.controller import Controller
-from clinic.patient import Patient
 from clinic.patient_record import PatientRecord
 from clinic.note import Note
 
 class PatientRecordTests(TestCase):
     
     def test_patient_record_create_note(self):
-        record = PatientRecord()   
-             
-        note1 = record.create_note("Patient comes with headache and high blood pressure.")
-        note_expected = Note(1, "Patient comes with headache and high blood pressure.")
-        note_not_expected = Note(2, "Patient comes with headache and high blood pressure")
+        
+        # set up test items
+        record = PatientRecord()
+        note1_text = "Patient comes with headache and high blood pressure."     
+        note1 = record.create_note(note1_text)
+        note_expected = Note(1, note1_text)
+        note_not_expected = Note(2, note1_text)
         
         # test properly created
+        self.assertIsNotNone(record.create_note(note1_text))
+        # test equality
         self.assertEqual(note1, note_expected)
         # test inequality
         self.assertNotEqual(note1, note_not_expected)
